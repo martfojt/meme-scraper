fee1f2f344f5234c0c4b8ae968d2bf3958bf8f36
Martin Fojtek <martfojt@gmail.com>
2024-01-11 16:09:38 +0100

    Add .catch to handle errors

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+import fs from 'node:fs';
+import http from 'node:http';
 import axios from 'axios';
-import fs from 'fs';
-import http from 'http';
 import { parse } from 'node-html-parser';
 
-//Use Axios to fetch HTML content from provided URL
+// Use Axios to fetch HTML content from provided URL
 axios
   .get('https://memegen-link-examples-upleveled.netlify.app/')
   .then(function (response) {
-    //Use node-html-parser to parse HTML content
+    // Use node-html-parser to parse HTML content
     const root = parse(response.data);
 
-    //Array for URLs
-    let imageUrls = [];
+    // Array for URLs
+    const imageUrls = [];
 
     // Select all image elements from HTML using node-html-parser
     const images = root.querySelectorAll('img');
 
     //
     images.forEach(function (img) {
-      let src = img.getAttribute('src');
+      const src = img.getAttribute('src');
       if (src) {
         imageUrls.push(src);
       }
@@ -26,7 +26,11 @@ axios
 
     // Loop through the first 10 URLs in the imageUrls array and name them
     for (let i = 0; i < Math.min(imageUrls.length, 10); i++) {
-      let currentUrl = imageUrls[i];
-      let filename = String(i + 1).padStart(2, '0') + '.jpg';
+      const currentUrl = imageUrls[i];
+      const filename = String(i + 1).padStart(2, '0') + '.jpg';
     }
+  })
+  // If there is .then, I must always use .catch to handle errors!!!
+  .catch(function (error) {
+    console.error('Error:', error);
   });
